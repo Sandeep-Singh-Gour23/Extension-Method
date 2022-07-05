eea95695b053ee3f886eeefb967903aa8faa454a
Sandeep-Singh-Gour23 <sandeepsinghgour0@gmail.com>
2022-07-05 15:09:52 +0530

    Add project files.

---
+++ ExtensionMethod.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace Extension_Method
+{
+    public class InvalidInputException : Exception
+    {
+        public InvalidInputException(String message)
+            : base(message)
+        {
+
+        }
+    }
+    public static class ExtensionMethod
+    {
+        public static string ToCurrency(this string str)
+        {
+            //var isNumeric = int.TryParse(str, out _);
+            var isNumeric = Regex.IsMatch(str, @"^\d+$");
+
+            if (isNumeric)
+                return "$" + str;
+
+            else
+                throw new InvalidInputException("Sorry, you have entered invalid value.");
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Extension_Method;
+
+namespace Extension_Method
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the value..............");
+            string input = Console.ReadLine();
+            string currency = ExtensionMethod.ToCurrency(input);
+            Console.WriteLine(currency);
+        }
+    }
+}
